7469ade3d5898d269f3b37c2b702685579f23fac
Byungkyu Ju <noel1227@gmail.com>
2023-02-24 18:30:59 +0900

    Time: 2 ms (56.27%), Space: 44.5 MB (54.97%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // O이면서, 외곽지점이 아닐 때 It is on the border that should not be flipped.
+                if (board[i][j] == 'O' && ( i == 0 || j == 0 || i == board.length -1 || j == board[0].length -1)) {
+                    dfs(board, i, j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O' ) {
+                    board[i][j] = 'X';
+                }else if (board[i][j] == 'V') { // 방문했던 V 는 X 로
+                    board[i][j] = 'O';
+                }
+                
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int i, int j) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') { //방문할 필요가 없으면 패스
+            return;
+        }
+    
+        board[i][j] = 'V'; //방문한 O를 V로
+        
+        // 오른쪽
+        dfs(board,i,j+1);    
+        // 왼쪽
+        dfs(board,i, j-1);
+        // 아래
+        dfs(board,i+1,j);
+        // 위
+        dfs(board,i-1,j);
+    }
+}
